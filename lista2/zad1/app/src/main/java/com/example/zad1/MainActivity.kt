9d5fe4decdbc7947873e7199be8fae25f2f9d3a6
JakubGogola <jakubgogola97@gmail.com>
2019-03-16 22:17:33 +0100

    wip

--- lista2/zad1/app/src/main/java/com/example/zad1/MainActivity.kt
+++ lista2/zad1/app/src/main/java/com/example/zad1/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.zad1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonClick(view: View) {
+        findViewById<TextView>(R.id.textView).text = view.id.toString()
+
     }
 }
