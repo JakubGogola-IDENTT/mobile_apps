dffc7ece67249e2e322bb69d6c348cd37cc225ca
JakubGogola <jakubgogola97@gmail.com>
2019-04-22 14:02:11 +0200

    xd

--- lista4/zad1/app/src/main/java/com/example/zad1/Image.kt
+++
@@ -1,5 +0,0 @@
-package com.example.zad1
-
-import java.io.Serializable
-
-data class Image (val url: String, val description: String, var ratng: Float) : Serializable
--- lista4/zad1/app/src/main/java/com/example/zad1/fragments/ImageFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.zad1.fragments
-
-import android.support.v4.app.Fragment
-
-class ImageFragment : Fragment() {
-
-}
